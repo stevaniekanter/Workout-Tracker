c7ac54a7ead73a29419802323dd13e635e54d4e5
Stevanie Kanter <stevaniek@yahoo.com>
2021-11-13 14:03:04 -0500

    updated connectiong

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
-  userCreateIndex: true,
 });
 
 // added routes
